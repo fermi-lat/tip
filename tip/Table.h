e63f18752e17bff4770015ae844b830e2aa786b6
peachey <>
2004-02-20 20:28:34 +0000

    Move Cell and Record "up" into Table's namespace.

--- tip/Table.h
+++ tip/Table.h
@@ -23,6 +23,84 @@ namespace table {
   */
   class Table {
     public:
+      class Record;
+
+      /** \class Cell
+
+          \brief Encapsulation of a single table cell, which may contain (in principle) any type of information.
+      */
+      class Cell {
+        public:
+          /** \brief Construct a Cell object, associated with the given Record.
+              \param record The Record object to which this Cell refers.
+              \param field The name of this cell.
+          */
+          Cell(Record & record, const std::string & field): m_record(record), m_field(field) {}
+
+          virtual ~Cell() {}
+
+          /** \brief Read the current value of this Cell from current Iterator position.
+              \param value The read value.
+          */
+          void read(double & value) const;
+
+        private:
+          Record & m_record;
+          std::string m_field;
+      };
+
+      /** \class Record
+
+          \brief Encapsulation of a single table record.
+      */
+      class Record {
+        public:
+          typedef std::map<std::string, Cell> CellCont_t;
+
+          /** \brief Construct a Record object, without association with any Table.
+          */
+          Record(): m_cells(), m_table(0), m_index(0) {}
+
+          /** \brief Construct a Record object, associated with the given Table and record number.
+          */
+          Record(Table & table, Index_t index): m_cells(), m_table(&table), m_index(index) {}
+
+          virtual ~Record() {}
+
+          Record & operator = (const Record & rec) {
+            if (this != &rec) {
+              // Note: do *not* assign m_cells! This is important to how Records work!
+              m_table = rec.m_table; // This should also confirm that the new table has the right fields.
+              m_index = rec.m_index;
+            }
+            return *this;
+          }
+
+          /** \brief Return a cell object for the given field. The Cell object will be created
+              if it does not already exist.
+
+              Note that there is no check at this point whether the underlying Table actually
+              has a field with this name.
+              \param field The name of the Cell object.
+          */
+          Cell & operator [](const std::string & field) {
+            CellCont_t::iterator itor = m_cells.find(field);
+            if (m_cells.end() == itor) {
+              itor = m_cells.insert(itor, std::make_pair(field, Cell(*this, field)));
+            }
+            return itor->second;
+          }
+
+          // Get the current row number and table. Client code should not normally need to call these.
+          Index_t getRowNum() const { return m_index; }
+          void nextRecord() { ++m_index; }
+          Table * getTable() { assert(m_table); return m_table; }
+
+        private:
+          CellCont_t m_cells;
+          Table * m_table;
+          Index_t m_index;
+      };
 
       /** \class Iterator
 
@@ -30,85 +108,6 @@ namespace table {
       */
       class Iterator {
         public:
-          class Record;
-
-          /** \class Cell
-
-              \brief Encapsulation of a single table cell, which may contain (in principle) any type of information.
-          */
-          class Cell {
-            public:
-              /** \brief Construct a Cell object, associated with the given Record.
-                  \param record The Record object to which this Cell refers.
-                  \param field The name of this cell.
-              */
-              Cell(Record & record, const std::string & field): m_record(record), m_field(field) {}
-
-              virtual ~Cell() {}
-
-              /** \brief Read the current value of this Cell from current Iterator position.
-                  \param value The read value.
-              */
-              void read(double & value) const;
-
-            private:
-              Record & m_record;
-              std::string m_field;
-          };
-
-          /** \class Record
-
-              \brief Encapsulation of a single table record.
-          */
-          class Record {
-            public:
-              typedef std::map<std::string, Cell> CellCont_t;
-
-              /** \brief Construct a Record object, without association with any Table.
-              */
-              Record(): m_cells(), m_table(0), m_index(0) {}
-
-              /** \brief Construct a Record object, associated with the given Table and record number.
-              */
-              Record(Table & table, Index_t index): m_cells(), m_table(&table), m_index(index) {}
-
-              virtual ~Record() {}
-
-              Record & operator = (const Record & rec) {
-                if (this != &rec) {
-                  // Note: do *not* assign m_cells! This is important to how Records work!
-                  m_table = rec.m_table; // This should also confirm that the new table has the right fields.
-                  m_index = rec.m_index;
-                }
-                return *this;
-              }
-
-              /** \brief Return a cell object for the given field. The Cell object will be created
-                  if it does not already exist.
-
-                  Note that there is no check at this point whether the underlying Table actually 
-                  has a field with this name.
-                  \param field The name of the Cell object.
-              */
-              Cell & operator [](const std::string & field) {
-                CellCont_t::iterator itor = m_cells.find(field);
-                if (m_cells.end() == itor) {
-                  itor = m_cells.insert(itor, std::make_pair(field, Cell(*this, field)));
-                }
-                return itor->second;
-              }
-
-              // Get the current row number and table. Client code should not normally need to call these.
-              Index_t getRowNum() const { return m_index; }
-              void nextRecord() { ++m_index; }
-              Table * getTable() { assert(m_table); return m_table; }
-
-            private:
-              CellCont_t m_cells;
-              Table * m_table;
-              Index_t m_index;
-          };
-
           /** \brief Create an Iterator object which is not immediately associated with a table. Assignment
               to this Iterator can form such an association.
           */
@@ -185,11 +184,11 @@ namespace table {
       ITabularData * m_tabular_data;
   };
 
-  inline void Table::Iterator::Cell::read(double & value) const
+  inline void Table::Cell::read(double & value) const
     { m_record.getTable()->read(m_field, m_record.getRowNum(), value); }
 
-  typedef Table::Iterator::Cell Cell;
-  typedef Table::Iterator::Record Record;
+  typedef Table::Cell Cell;
+  typedef Table::Record Record;
 }
 
 #endif
