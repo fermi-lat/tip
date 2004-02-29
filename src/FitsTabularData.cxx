e14a2f5f828041e0a73d1bab5e8f5c8863a8c8f9
peachey <>
2004-02-29 17:48:57 +0000

    Use new FitsExtensionData class to encapsulate parts of table access which are generic to all Fits extensions.

--- src/FitsTabularData.cxx
+++ src/FitsTabularData.cxx
@@ -13,19 +13,20 @@
 namespace table {
 
   FitsTabularData::FitsTabularData(const std::string & file_name, const std::string & table_name): ITabularData(),
-    m_file_name(file_name), m_table_name(table_name), m_fp(0), m_num_records(0) {
+    m_extension(file_name, table_name), m_col_info(), m_file_name(file_name), m_table_name(table_name),
+    m_fp(0), m_num_records(0) {
     int status = 0;
     long nrows = 0;
 
     // Open the actual file and move to the right table.
-    open();
+    fitsfile * fp = m_extension.open();
 
     // Read the number of rows present in the table.
-    fits_get_num_rows(m_fp, &nrows, &status);
+    fits_get_num_rows(fp, &nrows, &status);
 
     // Check for success and if not, do not continue.
     if (status || nrows < 0) {
-      close();
+      m_extension.close();
       throw TableException();
     }
 
@@ -38,20 +39,24 @@ namespace table {
       char name[128] = ""; // jp fix this: what is the maximum length of a FITS column name?
       int col_num = 0;
 
-      fits_get_colname(m_fp, CASEINSEN, match_all, name, &col_num, &status);
+      fits_get_colname(fp, CASEINSEN, match_all, name, &col_num, &status);
       if (0 == status || COL_NOT_FOUND == status || COL_NOT_UNIQUE == status) {
         for (char * itor = name; *itor; ++itor) *itor = tolower(*itor);
         m_col_info[name] = col_num;
       }
     }
     // Note: status variable is non-0 here, but that is OK -- just remember that if any code gets added here!
+
+    m_fp = fp;
   }
 
-  FitsTabularData::FitsTabularData(const FitsTabularData & table): ITabularData(), m_col_info(table.m_col_info),
-    m_file_name(table.m_file_name), m_table_name(table.m_table_name), m_fp(0), m_num_records(table.m_num_records) {
+  FitsTabularData::FitsTabularData(const FitsTabularData & table): ITabularData(), 
+    m_extension(table.m_file_name, table.m_table_name), m_col_info(table.m_col_info), m_file_name(table.m_file_name),
+    m_table_name(table.m_table_name), m_fp(0), m_num_records(table.m_num_records) {
     // Copy construction requires that the new table opens the file itself separately.
     try {
-      open();
+      fitsfile * fp = m_extension.open();
+      m_fp = fp;
     } catch(TableException & x) {
       m_col_info.clear();
       m_num_records = 0;
@@ -60,7 +65,6 @@ namespace table {
   }
 
   FitsTabularData::~FitsTabularData() {
-    close();
   }
 
   Index_t FitsTabularData::getNumRecords() const { return m_num_records; }
@@ -76,31 +80,8 @@ namespace table {
     fits_read_col(m_fp, TDOUBLE, col_num, record_index + 1, 1, 1, 0, &value, 0, &status);
   }
 
-  void FitsTabularData::open() {
-    fitsfile * fp = 0;
-    int status = 0;
-
-    // Open the fits file.
-    fits_open_file(&fp, const_cast<char *>(m_file_name.c_str()), READWRITE, &status);
-
-    if (status) throw TableException();
-
-    // Move to the indicated extension.
-    fits_movnam_hdu(fp, BINARY_TBL, const_cast<char *>(m_table_name.c_str()), 0, &status);
-
-    if (status) {
-      fits_close_file(fp, &status);
-      throw TableException();
-    }
-
-    // Success: save the pointer.
-    m_fp = fp;
-  }
-
-  void FitsTabularData::close() {
-    int status = 0;
-    if (m_fp) fits_close_file(m_fp, &status);
-    m_fp = 0;
+  void FitsTabularData::readKeyword(const std::string & name, double & value) const {
+    m_extension.readKeyword(name, value);
   }
 
 }
