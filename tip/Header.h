a488c737be35116065c11f8889226f8780552c4c
peachey <>
2004-02-29 17:30:57 +0000

    Use IExtensionData instead of IHeaderData.

--- tip/Header.h
+++ tip/Header.h
@@ -10,11 +10,10 @@
 #include <string>
 
 #include "table/Header.h"
+#include "table/IExtensionData.h"
 #include "table/table_types.h"
 
 namespace table {
-
-  class IHeaderData;
 
   /** \class Header
 
@@ -22,13 +21,15 @@ namespace table {
   */
   class Header {
     public:
+      Header(IExtensionData * extension_data): m_extension_data(extension_data) {}
 
-      Header(IHeaderData * header_data): m_header_data(header_data) {}
+      virtual ~Header() {}
 
-      virtual ~Header() { /* delete m_header_data; */ }
+      // The following function is only temporary:
+      void read(const std::string & name, double & value) { m_extension_data->readKeyword(name, value); }
 
     private:
-      IHeaderData * m_header_data;
+      IExtensionData * m_extension_data;
   };
 
 }
