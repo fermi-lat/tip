e2b32c6380e3a3e9a1695e4ef1e3269b831c74cb
peachey <>
2004-03-03 18:02:32 +0000

    Use new Keyword abstraction to handle keyword access.

--- tip/Header.h
+++ tip/Header.h
@@ -7,8 +7,10 @@
 #ifndef table_Header_h
 #define table_Header_h
 
+#include <map>
 #include <string>
 
+#include "table/Keyword.h"
 #include "table/Header.h"
 #include "table/IExtensionData.h"
 #include "table/table_types.h"
@@ -21,16 +23,29 @@ namespace table {
   */
   class Header {
     public:
-      Header(IExtensionData * extension_data): m_extension_data(extension_data) {}
+      typedef std::map<std::string, Keyword> KeywordCont_t;
 
-      virtual ~Header() {}
+      Header(IExtensionData * extension_data): m_keywords(), m_extension_data(extension_data) {}
 
-      // The following function is only temporary:
-      void getKeyword(const std::string & name, double & value) { m_extension_data->getKeyword(name, value); }
+      Keyword & operator [](const std::string & name) { return find_or_make(name); }
+
+      const Keyword & operator [](const std::string & name) const { return find_or_make(name); }
+
+    protected:
+      Keyword & find_or_make(const std::string & name) const;
 
     private:
+      KeywordCont_t m_keywords;
       IExtensionData * m_extension_data;
   };
+
+  inline Keyword & Header::find_or_make(const std::string & name) const {
+    Header & header = const_cast<Header &>(*this);
+    KeywordCont_t::iterator itor = header.m_keywords.find(name);
+    if (header.m_keywords.end() == itor)
+      itor = header.m_keywords.insert(itor, std::make_pair(name, Keyword(header.m_extension_data, name)));
+    return itor->second;
+  }
 
 }
 
