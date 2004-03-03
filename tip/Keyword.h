223df1bc2edf9b645c3e4748aed9d6145624b5a8
peachey <>
2004-03-03 18:02:08 +0000

    Abstraction to handle keywords.

---
+++ tip/Keyword.h
@@ -0,0 +1,45 @@
+/** \file Keyword.h
+
+    \brief Helper classes to allow convenient and idiomatic access to ITabularData objects.
+
+    \author James Peachey, HEASARC
+*/
+#ifndef table_Keyword_h
+#define table_Keyword_h
+
+#include "table/IExtensionData.h"
+#include "table/TableException.h"
+
+namespace table {
+
+  /** \class Keyword
+
+      \brief Encapsulation of a single keyword.
+
+      The get() method will need to be overloaded for every type the Keyword may in practice contain.
+  */
+  class Keyword {
+    public:
+      /** \brief Construct a Keyword object associated with a particular Header.
+          \param extension_data Pointer to the referent IExtensionData object.
+          \param name The name of this Keyword.
+      */
+      Keyword(IExtensionData * extension_data, const std::string & name): m_extension_data(extension_data),
+        m_name(name) {}
+
+      /** \brief Get the current value of this Keyword.
+          \param value The current value.
+      */
+      void get(double & value) const;
+
+    private:
+      IExtensionData * m_extension_data;
+      std::string m_name;
+  };
+
+  // Keyword
+  inline void Keyword::get(double & value) const { m_extension_data->getKeyword(m_name, value); }
+
+}
+
+#endif
