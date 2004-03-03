502730aec9f4c23e78702f3cb3b3c43bcbbb9a7e
peachey <>
2004-03-03 20:40:28 +0000

    Change scheme for getting columns. The first time a value is needed from a column, the column number is determined, and thereafter the column number is used directly to get the data. This involved adding a new method getFieldIndex to ITabularData.h to get the column (index) number. Also the signature of getCell was changed to use the index number instead of a string to find its values.

--- tip/ITabularData.h
+++ tip/ITabularData.h
@@ -26,15 +26,18 @@ namespace table {
       */
       virtual Index_t getNumRecords() const = 0;
 
+      /** \brief Get an index associated with the given field (column) name.
+          \param field_name The name of the field.
+      */
+      virtual FieldIndex_t getFieldIndex(const std::string & field_name) const = 0;
+
       /** \brief Get a value from the current tabular data object.
-          \param field The name of the field (column) to get.
+          \param field_index The index of the field (column) to get.
           \param record_index The index whose value to get.
           \param value The output value.
       */
-      virtual void getCell(const std::string & field, Index_t record_index, double & value) const = 0;
-      virtual void getCell(const std::string & field, Index_t record_index, signed long & value) const = 0;
-
-      virtual FieldIndex_t getFieldIndex(const std::string & field) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, double & value) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, signed long & value) const = 0;
   };
 
 }
