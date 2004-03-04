15a8264900fd58ca9ecf200a377663226fe6594f
peachey <>
2004-03-04 18:52:47 +0000

    Move getCellGeneric templates to FitsTabularData class.

--- src/FitsExtension.h
+++ src/FitsExtension.h
@@ -36,15 +36,6 @@ namespace table {
       */
       ~FitsExtension();
 
-      /** \brief Templated function which can get any kind of data from a FITS table. This
-          method throws an exception if the extension is not a table.
-          \param col_num The number of the column.
-          \param record_index The record (row) number.
-          \param value The variable in which the read value is placed.
-      */
-      template <typename T>
-      void getCellGeneric(int col_num, Index_t record_index, T & value) const;
-
       /** \brief Templated function which can get keywords from a FITS table, converted to any data type.
           \param name The name of the keyword.
           \param value The variable in which the read value is placed.
@@ -65,54 +56,27 @@ namespace table {
       fitsfile * getFitsFp() const { return m_fp; }
 
     private:
-      static const FieldIndex_t s_max_scalar_len = 64;
       std::string m_file_name;
       std::string m_ext_name;
       fitsfile * m_fp;
   };
 
-  // Getting columns.
-  template <typename T>
-  inline void FitsExtension::getCellGeneric(int col_num, Index_t record_index, T & value) const {
-    int status = 0;
-    fits_read_col(m_fp, FitsPrimProps<T>::dataTypeCode(), col_num, record_index + 1, 1, 1, 0, &value, 0, &status);
-    if (status) throw TableException();
-  }
-
-  // Getting column values as bools is a special case because Cfitsio gets them as ints.
-  template <>
-  inline void FitsExtension::getCellGeneric<bool>(int col_num, Index_t record_index, bool & value) const {
-    int status = 0;
-    int tmp = 0;
-    fits_read_col(m_fp, FitsPrimProps<bool>::dataTypeCode(), col_num, record_index + 1, 1, 1, 0, &tmp, 0, &status);
-    if (status) throw TableException();
-    value = tmp;
-  }
-
-  // Getting column values as strings is a special case because Cfitsio gets them as char *.
-  template <>
-  inline void FitsExtension::getCellGeneric<std::string>(int col_num, Index_t record_index, std::string & value) const {
-    int status = 0;
-    char tmp[s_max_scalar_len];
-    fits_read_col(m_fp, FitsPrimProps<std::string>::dataTypeCode(), col_num, record_index + 1, 1, 1, 0, tmp, 0, &status);
-    if (status) throw TableException();
-    value = tmp;
-  }
-
   // Getting keywords.
   template <typename T>
   inline void FitsExtension::getKeywordGeneric(const std::string & name, T & value) const {
+    static int data_type_code = FitsPrimProps<T>::dataTypeCode();
     int status = 0;
-    fits_read_key(m_fp, FitsPrimProps<T>::dataTypeCode(), const_cast<char *>(name.c_str()), &value, 0, &status);
+    fits_read_key(m_fp, data_type_code, const_cast<char *>(name.c_str()), &value, 0, &status);
     if (status) throw TableException();
   }
 
   // Getting keywords as bool is a special case because Cfitsio gets them as ints.
   template <>
   inline void FitsExtension::getKeywordGeneric<bool>(const std::string & name, bool & value) const {
+    static int data_type_code = FitsPrimProps<bool>::dataTypeCode();
     int status = 0;
     int tmp = 0;
-    fits_read_key(m_fp, FitsPrimProps<bool>::dataTypeCode(), const_cast<char *>(name.c_str()), &tmp, 0, &status);
+    fits_read_key(m_fp, data_type_code, const_cast<char *>(name.c_str()), &tmp, 0, &status);
     if (status) throw TableException();
     value = tmp;
   }
@@ -120,9 +84,10 @@ namespace table {
   // Getting keywords as strings is a special case because Cfitsio gets them as char *.
   template <>
   inline void FitsExtension::getKeywordGeneric<std::string>(const std::string & name, std::string & value) const {
+    static int data_type_code = FitsPrimProps<std::string>::dataTypeCode();
     int status = 0;
     char tmp[FLEN_KEYWORD];
-    fits_read_key(m_fp, FitsPrimProps<std::string>::dataTypeCode(), const_cast<char *>(name.c_str()), tmp, 0, &status);
+    fits_read_key(m_fp, data_type_code, const_cast<char *>(name.c_str()), tmp, 0, &status);
     if (status) throw TableException();
     value = tmp;
   }
