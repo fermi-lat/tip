5c1554fbfd3a5b228f582c12c3985872ff586b37
peachey <>
2004-03-04 18:51:18 +0000

    Move getCellGeneric here from FitsExtension class. Add getVectorCellGeneric for low level support for vector columns. Add new internal class ColumnInfo for encapsulating info about the columns. When opening a table, populate two containers for this info: one indexed on column number, the other on column name.

--- src/FitsTabularData.cxx
+++ src/FitsTabularData.cxx
@@ -13,8 +13,9 @@
 namespace table {
 
   FitsTabularData::FitsTabularData(const std::string & file_name, const std::string & table_name): ITabularData(),
-    m_extension(file_name, table_name), m_col_info(), m_file_name(file_name), m_table_name(table_name),
-    m_num_records(0) {
+    m_extension(file_name, table_name), m_col_name_lookup(), m_col_num_lookup(), m_file_name(file_name),
+    m_table_name(table_name), m_num_records(0) {
+    int column_status = 0;
     int status = 0;
     long nrows = 0;
 
@@ -34,28 +35,45 @@ namespace table {
     m_num_records = (Index_t) nrows;
 
     // Iterate over columns, putting the name of each in the column container.
-    while (COL_NOT_FOUND != status) {
+    while (COL_NOT_FOUND != column_status) {
       char * match_all = "*";
       char name[128] = ""; // jp fix this: what is the maximum length of a FITS column name?
       int col_num = 0;
+      long repeat = 0;
 
-      fits_get_colname(fp, CASEINSEN, match_all, name, &col_num, &status);
-      if (0 == status || COL_NOT_FOUND == status || COL_NOT_UNIQUE == status) {
+      // Get each column's name.
+      fits_get_colname(fp, CASEINSEN, match_all, name, &col_num, &column_status);
+      if (0 == column_status || COL_NOT_UNIQUE == column_status) {
         for (char * itor = name; *itor; ++itor) *itor = tolower(*itor);
-        m_col_info[name] = col_num;
+
+        // Also get its repeat count.
+        fits_get_coltype(fp, col_num, 0, &repeat, 0, &status);
+        if (0 != status) {
+          m_extension.close();
+          throw TableException();
+        }
+
+        // Save values iff successful getting all the information.
+        m_col_name_lookup[name].m_name = name;
+        m_col_name_lookup[name].m_col_num = col_num;
+        m_col_name_lookup[name].m_repeat = repeat;
+        m_col_num_lookup[col_num].m_name = name;
+        m_col_num_lookup[col_num].m_col_num = col_num;
+        m_col_num_lookup[col_num].m_repeat = repeat;
       }
     }
-    // Note: status variable is non-0 here, but that is OK -- just remember that if any code gets added here!
   }
 
   FitsTabularData::FitsTabularData(const FitsTabularData & table): ITabularData(), 
-    m_extension(table.m_file_name, table.m_table_name), m_col_info(table.m_col_info), m_file_name(table.m_file_name),
-    m_table_name(table.m_table_name), m_num_records(table.m_num_records) {
+    m_extension(table.m_file_name, table.m_table_name), m_col_name_lookup(table.m_col_name_lookup),
+    m_col_num_lookup(table.m_col_num_lookup), m_file_name(table.m_file_name), m_table_name(table.m_table_name),
+    m_num_records(table.m_num_records) {
     // Copy construction requires that the new table opens the file itself separately.
     try {
       m_extension.open();
     } catch(TableException & x) {
-      m_col_info.clear();
+      m_col_name_lookup.clear();
+      m_col_num_lookup.clear();
       m_num_records = 0;
       throw;
     }
@@ -72,60 +90,64 @@ namespace table {
     for (std::string::iterator itor = tmp.begin(); itor != tmp.end(); ++itor) *itor = tolower(*itor);
 
     // Find (lowercased) field_name in container of columns. Complain if not found.
-    std::map<std::string, int>::const_iterator itor = m_col_info.find(tmp);
-    if (itor == m_col_info.end()) throw TableException();
+    std::map<std::string, ColumnInfo>::const_iterator itor = m_col_name_lookup.find(tmp);
+    if (itor == m_col_name_lookup.end()) throw TableException();
 
     // Get the number of the column.
-    return itor->second;
+    return itor->second.m_col_num;
   }
 
   // getCell overloads.
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, bool & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, double & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, float & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, char & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, signed char & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, signed short & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, signed int & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, signed long & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, unsigned char & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, unsigned short & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, unsigned int & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, unsigned long & value) const {
-    m_extension.getCellGeneric(field_index, record_index, value);
+    getCellGeneric(field_index, record_index, value);
+  }
+
+  void FitsTabularData::getCell(FieldIndex_t field_index, Index_t record_index, std::vector<double> & value) const {
+    getVectorCellGeneric(field_index, record_index, value);
   }
 
   void FitsTabularData::getKeyword(const std::string & name, double & value) const {
