93a75e5e1213eb3fdc05c33e3b2744958e7b2de4
peachey <>
2004-03-05 14:17:59 +0000

    Add support for getting vector values from columns.

--- tip/ITabularData.h
+++ tip/ITabularData.h
@@ -53,6 +53,39 @@ namespace table {
       virtual void getCell(FieldIndex_t field_index, Index_t record_index, unsigned long & value) const = 0;
 
       virtual void getCell(FieldIndex_t field_index, Index_t record_index, std::vector<double> & value) const = 0;
+
+      /** \brief Get one or more values from the current tabular data object.
+          \param field_index The index of the field (column) to get.
+          \param record_index The record index (row number) whose value to get.
+          \param src_begin Index of the first element within the Cell.
+          \param src_end Index of one element past the last element within the Cell.
+          \param dest_begin Pointer to the first element in the output sequence.
+      */
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        bool * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        double * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        float * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        char * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        signed char * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        signed short * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        signed int * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        signed long * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        unsigned char * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        unsigned short * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        unsigned int * dest_begin) const = 0;
+      virtual void getCell(FieldIndex_t field_index, Index_t record_index, Index_t src_begin, Index_t src_end,
+        unsigned long * dest_begin) const = 0;
+
   };
 
 }
