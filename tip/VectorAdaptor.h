018a6d6f6a5263ca7200c4d7b412d0816adb096f
peachey <>
2004-03-08 21:59:57 +0000

    Fix get/set for vector table cells.

--- tip/VectorAdaptor.h
+++ tip/VectorAdaptor.h
@@ -24,30 +24,32 @@ namespace table {
     public:
       class Entry {
         public:
-          Entry(VectorAdaptor & adaptor, T * current): m_adaptor(adaptor), m_current(current) {}
-          Entry & operator =(const T & data) { *m_current = data; m_adaptor.setModified(); return *this; }
-          operator const T &() const { return *m_current; }
-          T * getCurrent() { return m_current; }
-          const T * getCurrent() const { return m_current; }
-          void setCurrent(T * current) { m_current = current; }
-          Entry & itorNext() { ++m_current; return *this; }
-          Entry & itorPrev() { --m_current; return *this; }
-          bool itorEquals(const Entry & entry) const { return m_current == entry; }
-          bool itorLessThan(const Entry & entry) const { return m_current < entry; }
-          bool itorGreaterThan(const Entry & entry) const { return m_current > entry; }
+          Entry(VectorAdaptor & adaptor, Index_t current_index): m_adaptor(adaptor), m_index(current_index) {}
+          Entry & operator =(const T & data) { m_adaptor.set(m_index, data); return *this; }
+          operator const T &() const { return m_adaptor.get(m_index); }
+          Index_t getIndex() { return m_index; }
+          void setIndex(Index_t current_index) { m_index = current_index; }
+          Entry & itorNext() { ++m_index; return *this; }
+          Entry & itorPrev() { --m_index; return *this; }
+          bool itorEquals(const Entry & entry) const
+            { return &m_adaptor == &entry.m_adaptor && m_index == entry.m_index; }
+          bool itorLessThan(const Entry & entry) const
+            { return &m_adaptor == &entry.m_adaptor && m_index < entry.m_index; }
+          bool itorGreaterThan(const Entry & entry) const
+            { return &m_adaptor == &entry.m_adaptor && m_index > entry.m_index; }
 
         private:
           VectorAdaptor & m_adaptor;
-          T * m_current;
+          Index_t m_index;
       };
 
 //      typedef RandomAccessIterator<Entry, IndexDiff_t> Iterator;
 
-      VectorAdaptor(Referent & referent): m_referent(&referent), m_entry(*this, 0), m_begin(0), m_end(0), m_modified(false) {}
+      VectorAdaptor(Referent & referent): m_referent(&referent), m_entry(*this, 0), m_begin(0), m_end(0) {}
 
       // Need to fix this:
       VectorAdaptor(const VectorAdaptor & adaptor): m_referent(adaptor.m_referent), m_entry(*this, 0), m_begin(0),
-        m_end(0), m_modified(false) { assert(0); }
+        m_end(0) { assert(0); }
 
       ~VectorAdaptor() { delete [] m_begin; }
 
@@ -59,35 +61,49 @@ namespace table {
       const Entry & operator [](Index_t entry_index) const
         { VectorAdaptor & self = const_cast<VectorAdaptor &>(*this); return self.getEntry(entry_index); }
 
-      void setModified(bool modified = true) { m_modified = modified; }
-
       void allocate();
+
+      const T & get(Index_t entry_index);
 
       Entry & getEntry(Index_t entry_index);
 
       Index_t getNumElements() const { return m_referent->getNumElements(); }
+
+      void set(Index_t entry_index, const T & value);
 
     private:
       Referent * m_referent;
       Entry m_entry;
       T * m_begin;
       T * m_end;
-      bool m_modified;
   };
 
   template <typename T, typename Referent>
-  void VectorAdaptor<T, Referent>::allocate() {
+  inline void VectorAdaptor<T, Referent>::allocate() {
     Index_t vec_size = m_referent->getNumElements();
     m_begin = new T[vec_size];
     m_end = m_begin + vec_size;
   }
 
   template <typename T, typename Referent>
-  typename VectorAdaptor<T, Referent>::Entry & VectorAdaptor<T, Referent>::getEntry(Index_t entry_index) {
+  inline const T & VectorAdaptor<T, Referent>::get(Index_t entry_index) {
     if (!m_begin) allocate();
     m_referent->get(0, m_end - m_begin, m_begin);
-    m_entry.setCurrent(m_begin + entry_index);
+    return m_begin[entry_index];
+  }
+
+  template <typename T, typename Referent>
+  inline typename VectorAdaptor<T, Referent>::Entry & VectorAdaptor<T, Referent>::getEntry(Index_t entry_index) {
+    if (!m_begin) allocate();
+    m_referent->get(0, m_end - m_begin, m_begin);
+    m_entry.setIndex(entry_index);
     return m_entry;
+  }
+
+  template <typename T, typename Referent>
+  inline void VectorAdaptor<T, Referent>::set(Index_t entry_index, const T & value) {
+    if (!m_begin) allocate();
+    m_referent->set(entry_index, entry_index + 1, m_begin + entry_index);
   }
 
 }
