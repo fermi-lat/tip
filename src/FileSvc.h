2fc60f2c4833ecc779190caa05ac3f5194235104
peachey <>
2004-03-19 21:27:39 +0000

    Rename FitsFileSvc to FileSvc.

--- src/FileSvc.h
+++ src/FileSvc.h
@@ -1,34 +1,31 @@
-/** \file FitsFileSvc.h
+/** \file FileSvc.h
 
-    \brief FITS-specific I/O class. This functions as a factory for creating
-    data objects which are associated with FITS files.
+    \brief Declaration of I/O singleton factory class. This functions as a factory for creating
+    data objects which are associated with FITS or Root files.
 
     \author James Peachey, HEASARC
 */
-#ifndef table_FitsFileSvc_h
-#define table_FitsFileSvc_h
+#ifndef table_FileSvc_h
+#define table_FileSvc_h
 
 #include "table/IFileSvc.h"
 
 namespace table {
 
-  /** \class FitsFileSvc
+  /** \class FileSvc
 
       \brief Singleton factory for creating, editing and reading tables and images from files.
-      Note that despite the FITS-specific name, this will (for now at least) also try to open
-      Root files! This class should thus be renamed eventually, or else a Root-specific FileSvc
-      developed
   */
-  class FitsFileSvc : public IFileSvc {
+  class FileSvc : public IFileSvc {
     public:
 
-      /** \brief Create a new I/O accessor for FITS files.
+      /** \brief Create a new I/O accessor for files.
       */
-      FitsFileSvc();
+      FileSvc();
 
-      /** \brief Open an existing FITS table or Root table (for now) with modification access.
-          \param file_name The name of the FITS file.
-          \param table_name The name of the FITS table.
+      /** \brief Open an existing table with modification access.
+          \param file_name The name of the file containing the table.
+          \param table_name The name of the table.
       */
       virtual Table * editTable(const std::string & file_name, const std::string & table_name);
   };
