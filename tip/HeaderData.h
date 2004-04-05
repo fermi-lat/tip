c2b32115e6d8aa82b0566ee2e7e1f7c296a71129
peachey <>
2004-04-05 22:10:44 +0000

    Complete overloads for getKeyword and setKeyword.

--- tip/HeaderData.h
+++ tip/HeaderData.h
@@ -35,11 +35,65 @@ namespace tip {
           \param name The name of the keyword to get from the header object.
           \param value The output value of the keyword, converted to the given type.
       */
+      virtual void getKeyword(const std::string & name, bool & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
       virtual void getKeyword(const std::string & name, double & value) const
         { m_implementor.getKeywordGeneric(name, value); }
-
+      virtual void getKeyword(const std::string & name, float & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, char & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, signed char & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, signed short & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, signed int & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, signed long & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, unsigned char & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, unsigned short & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, unsigned int & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
+      virtual void getKeyword(const std::string & name, unsigned long & value) const
+        { m_implementor.getKeywordGeneric(name, value); }
       virtual void getKeyword(const std::string & name, std::string & value) const
         { m_implementor.getKeywordGeneric(name, value); }
+
+      /** \brief Set a keyword in this header object.
+          \param name The name of the keyword to get from the header object.
+          \param value The output value of the keyword, converted to the given type.
+      */
+      virtual void setKeyword(const std::string & name, const bool & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const double & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const float & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const char & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const signed char & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const signed short & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const signed int & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const signed long & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const unsigned char & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const unsigned short & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const unsigned int & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const unsigned long & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const std::string & value)
+        { m_implementor.setKeywordGeneric(name, value); }
+      virtual void setKeyword(const std::string & name, const char * const & value)
+        { m_implementor.setKeywordGeneric(name, value); }
 
     private:
       Implementor & m_implementor;
