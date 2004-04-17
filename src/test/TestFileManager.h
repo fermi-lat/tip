d5391e4c209b4593cd9e9d349b79c762cc3548db
peachey <>
2004-04-17 07:07:11 +0000

    Tests for file oriented activities.

---
+++ src/test/TestFileManager.h
@@ -0,0 +1,29 @@
+/** \file TestFileManager.h
+    \brief Declaration for class to perform detailed testing of file manager-related classes.
+    \author James Peachey, HEASARC
+*/
+#ifndef tip_TestFileManager_h
+#define tip_TestFileManager_h
+
+#include <string>
+
+#include "TestHarness.h"
+
+namespace tip {
+
+  /** \class TestFileManager
+      \brief Declaration for class to perform detailed testing of file manager-related classes.
+  */
+  class TestFileManager : public TestHarness {
+    public:
+      /** \brief Perform the detailed test needed by the subobject.
+      */
+      virtual int test(int status);
+
+      /// \brief Test createFile method.
+      void createFileTest();
+  };
+
+}
+
+#endif
