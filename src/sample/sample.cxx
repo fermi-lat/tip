d7531ca59d33ccd420ece43d1093529444fa345b
peachey <>
2004-04-22 15:39:10 +0000

    Modify sample code to demonstrate const Table access.

--- src/sample/sample.cxx
+++ src/sample/sample.cxx
@@ -1,5 +1,7 @@
 /** \file sample.cxx
     \brief Sample application which illustrates the points discussed in the mainpage.h.
+    Note that this is only a demonstration of how to use tip. It is not a good example of
+    how to write an application! For that see the st_app and st_app_examples packages.
     \author  James Peachey peachey@lheamail.gsfc.nasa.gov
 */
 
@@ -11,75 +13,89 @@
 #include "tip/Table.h"
 
 int main() {
+  using namespace tip;
+  const Table * const_table = 0;
   try {
     double silence_warning;
 
     // Example 1:
-    using namespace tip;
-    Table * my_table = IFileSvc::instance().editTable("day023.fits", "LAT_Event_Summary");
-  
+    const_table = IFileSvc::instance().readTable("day023.fits", "LAT_Event_Summary");
+
     // Example 2:
-    Header & header = my_table->getHeader();
+    const Header & header = const_table->getHeader();
     double tstart;
     header["tstart"].get(tstart);
-  
+
     // Example 3:
     // Loop over all records (rows) and extract values of ph_time column.
-    for (Table::Iterator itor = my_table->begin(); itor != my_table->end(); ++itor) {
-  
+    for (Table::ConstIterator itor = const_table->begin(); itor != const_table->end(); ++itor) {
+
       // Local double variable to hold the value of the field for each row:
       double ph_time_dbl = (*itor)["ph_time"].get();
-  
+
       // Do something useful with ph_time_dbl's value here ...
       // (The following is just to silence a compiler warning:)
       silence_warning += ph_time_dbl;
     }
-  
+
     // Example 4:
-    // Completely equivalent to Example 3:
+    // Completely equivalent to Example 3, just provides explicit typenames.
     // Loop over all records (rows) and extract values of ph_time column.
-    for (Table::Iterator itor = my_table->begin(); itor != my_table->end(); ++itor) {
-  
+    for (Table::ConstIterator itor = const_table->begin(); itor != const_table->end(); ++itor) {
+
       // Dereference the iterator and bind it to a local reference:
-      Table::Record & record = *itor;
-  
+      Table::ConstRecord & record = *itor;
+
       // Create a local reference representing the field (ph_time_cell) of
       // interest:
-      Table::Cell & ph_time_cell(record["ph_time"]);
-  
+      const Table::Cell & ph_time_cell(record["ph_time"]);
+
       // Get the current value:
       double ph_time_dbl = ph_time_cell.get();
-  
+
       // Do something useful with ph_time_dbl's value here ...
       // (The following is just to silence a compiler warning:)
       silence_warning += ph_time_dbl;
     }
-  
+
+  } catch (const std::exception & x) {
+    std::cerr << "Caught " << typeid(x).name() << ": " << x.what() << std::endl;
+  }
+  delete const_table;
+
+  Table * table = 0;
+  try {
     // Example 5:
+    using namespace tip;
+    table = IFileSvc::instance().editTable("day023.fits", "LAT_Event_Summary");
+
+    Header & header = table->getHeader();
+    double tstart;
+    header["tstart"].get(tstart);
+
     // Modify tstart keyword:
     double offset = 86400.;
     tstart -= offset;
     header["tstart"].set(tstart);
-  
+
     // Modify ph_time column:
     // Loop over all records (rows), extract and modify values of ph_time column.
-    for (Table::Iterator itor = my_table->begin(); itor != my_table->end(); ++itor) {
-  
+    for (Table::Iterator itor = table->begin(); itor != table->end(); ++itor) {
+
       // Local double variable to hold the value of the field for each row:
       double ph_time_dbl = (*itor)["ph_time"].get();
-  
+
       // Modify value:
       ph_time_dbl -= offset;
-  
+
       // Write it back to the table:
       (*itor)["ph_time"].set(ph_time_dbl);
     }
 
   } catch (const std::exception & x) {
     std::cerr << "Caught " << typeid(x).name() << ": " << x.what() << std::endl;
-  } catch (...) {
-    std::cerr << "Caught unknown object." << std::endl;
   }
+  delete table;
 
   return 0;
 }
