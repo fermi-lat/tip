5b346d73492774682c94663e1818d0077d515af6
Tom Stephens <thomas.stephens@gmail.com>
2019-12-06 11:28:55 -0700

    Fixed tabs and indention issues

--- tipLib.py
+++ tipLib.py
@@ -1,10 +1,10 @@
 #$Id: tipLib.py,v 1.2 2008/02/26 16:27:13 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
-	env.Tool('addLibrary', library = ['tip'])
-	if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
-	    env.Tool('findPkgPath', package = 'tip') 
-	    env.Tool('findPkgPath', package = 'facilities') 
+        env.Tool('addLibrary', library = ['tip'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'tip') 
+        env.Tool('findPkgPath', package = 'facilities') 
     if kw.get('incsOnly', 0) == 1:
         env.Tool('findPkgPath', package = 'facilities')         
         return
